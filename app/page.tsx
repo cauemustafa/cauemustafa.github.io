55928e4f63cd0d6d9cb414ecd51fa93095cdface
ibelick <julien.thibeaut@gmail.com>
2024-12-19 09:45:45 +0100

    feat: init repo next 15, react 19, tailwind css v4

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="bg-gray-5 p-4 rounded-lg">
+        <h1 className="text-lg text-gray-1">Hello, World!</h1>
+      </div>
+    </div>
+  );
+}
